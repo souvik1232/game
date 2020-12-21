950e662a0a75ba146b6b3e66d0a5611303bf259e
souvik1232 <souvik199831@gmail.com>
2020-12-21 11:29:23 +0530

    Added:Ability to choose between X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -15,9 +15,27 @@ public class TicTacToeGame {
         }
         return board;
     }
-
+    //UserCase2
+    public static String[] inputBoard(String player,String computer)
+    {
+        String[] Choice = new String[2];
+        System.out.println("Choose Between X or O");
+        player = scanner.next().toUpperCase();//Asking user input choice
+        if(player.equals("X")) {
+            computer = "O";
+        }
+        else if (player.equals("O")) {
+            computer = "X";
+        }
+        Choice[0] = player;
+        Choice[1] = computer;
+        return Choice;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe");
+        String player = null;
+        String computer = null;
         char[] board = createBoard();
+        String[] input = inputBoard(player,computer);
     }
 }
