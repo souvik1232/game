340d65ee6ee07f6d7a865f0472a64cfb7e53cd90
souvik1232 <souvik199831@gmail.com>
2020-12-21 11:25:49 +0530

    Added:Board of char array size 9

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -18,6 +18,6 @@ public class TicTacToeGame {
 
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe");
-//        char[] board = createBoard();
+        char[] board = createBoard();
     }
 }
